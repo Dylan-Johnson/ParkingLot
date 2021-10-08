a3db40a458ca290ed7b3aad5738287fcc4305a07
Dylan Johnson <44678812+Dylan-Johnson@users.noreply.github.com>
2021-10-08 16:45:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
+from flask import Flask, render_template, redirect
 import json
-
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
-Lot = [[101, -1],[102, -1], [103, -1], [104, -1], [105, -1]]
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/changeSpace/<spaceNo>')
-def changeSpace(spaceNo):
-    print(Lot)
-    for x in Lot:
-        if x[0] == int(spaceNo):
-            x[1] = -(x[1])
-    print(Lot)
-    return render_template('base.html', num=200)
+@app.route('/space/<space>')
+def space(space):
 
-@app.route('/getParking')
-def getParking():
-    return render_template('lot.html', lotNo=Lot, num=200)
+    # Grabs the space and changes the status
+    mySpace = int(space)
+    testList[mySpace-1][5] *= -1
 
+    #return space
+    return redirect("/testCanvasLoad")
+
+# Our parking lot data structure
+# x, y, width, height, number, status
+testList = [[20, 20, 150, 100, 1, 1], [250, 250, 150, 100, 2, -1],[20, 400, 150, 100, 3, 1]]
+
+
+# Loads the parking lot
+@app.route('/testCanvasLoad')
+def testCanvasLoad():
+    return render_template('canvas.html', list=testList)
+
+# Grabs JSON data of our parking lot for AJAX in our website
 @app.route('/getdata')
 def getdata():
-    return (json.dumps(Lot))
+    return (json.dumps(testList))
 
 if __name__ == '__main__':
     app.run()
-
